85122d39e42eea4b97e18d41c7d3662e4b138e96
defnebulut <d.bulut0907@gmail.com>
2022-05-14 19:02:10 +0300

    left menu

---
+++ manager/managerMenu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/menu.css" />
+    <title>Document</title>
+</head>
+<body>
+<div style="margin-top: 40%">
+        <a class="mang-menu" href="departments.php">Departments</a>
+        <a class="mang-menu" href="reservations.php">Reservations</a>
+        <a class="mang-menu" href="vehicles.php">Vehicles</a>
+    </div>
+</body>
+</html>
+
+
