97f73ab8bab31844a9c7f4911c40eb0ccc8fe6f6
defnebulut <d.bulut0907@gmail.com>
2022-05-14 19:02:32 +0300

    right menu

---
+++ manager/managerMenu2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+  <title>Document</title>
+</head>
+<body>
+<div class='post-it' contenteditable>
+        <h1>To Do List</h1>
+        <ul>
+          <li></li>
+        </ul>
+
+      </div>
+      <hr class="solid" width="90%" style="margin: auto; margin-top: 90px;">
+      <h3 style="text-align: center; margin: 30px 0 20px 0;">Helpful Links:</h3>
+      <div style="text-align: center;">
+        <a class="mang-menu" id="smaller-menu" href="#">How to do something?</a>
+        <a class="mang-menu" id="smaller-menu" href="#">How to do another thing?</a>
+        <a class="mang-menu" id="smaller-menu" href="#">How to do another thing?</a>
+        <a class="mang-menu" id="smaller-menu" href="#">How to do another thing?</a>
+      </div>
+</body>
+</html>
