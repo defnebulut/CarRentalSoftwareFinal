aeb52620e2067150ecee7a0456f31479d1aae542
defnebulut <d.bulut0907@gmail.com>
2022-05-14 18:59:57 +0300

    import issues fixed

--- manager/navbar.php
+++ manager/navbar.php
@@ -13,7 +13,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <script src="https://kit.fontawesome.com/d540061d63.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
-    <link rel="stylesheet" href="css/style.css" />
+    
 </head>
 
 <body>
