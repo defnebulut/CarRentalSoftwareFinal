c4df50c3980e271b1b8bfe0ecb80cdc48344b29e
defnebulut <d.bulut0907@gmail.com>
2022-05-15 04:16:35 +0300

    account settings page

---
+++ manager/accountSettings.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css" />
+    <title>Account Settings</title>
+</head>
+
+<body>
+    <?php include "navbar.php"; ?>
+    <div class="row g-0">
+        <div class="col-6 col-md-2" style="background-color:  #8f92962f; display: inline;">
+            <?php include "managerMenu.php"; ?>
+        </div>
+        <div class="col-sm-6 col-md-8">
+            <h2 style="text-align: center; margin: 70px 0 40px 10px">ACCOUNT DETAILS</h2>
+        </div>
+        <div class="col-6 col-md-2" style="background-color:  #8f92962f; display: inline;">
+            <?php include "managerMenu2.php"; ?>
+        </div>
+    </div>
+    <?php include "footer.php"; ?>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
